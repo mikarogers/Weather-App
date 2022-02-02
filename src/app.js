57a96d934121193416cab9a81feafb8f221db864
mikarogers <96159267+mikarogers@users.noreply.github.com>
2022-02-02 13:17:24 -0800

    Added search engine

--- src/app.js
+++ src/app.js
@@ -61,8 +61,18 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "3bb1078cb3d49614deb769ef35761e96";
-let city = "Ottawa";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "3bb1078cb3d49614deb769ef35761e96";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
