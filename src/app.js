ba2e94bcd744707a970be3537a24e5cbe380478e
mikarogers <96159267+mikarogers@users.noreply.github.com>
2022-02-03 11:08:17 -0800

    Added JS template for forecast

--- src/app.js
+++ src/app.js
@@ -40,6 +40,30 @@ function formatDate(timestamp) {
   return `${day}, ${month} ${date} ${hour}:${minute}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Wed", "Thu", "Fri", "Sat"];
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+                <div class="weather-forecast-date">${day}</div>
+                <img
+                  src="http://openweathermap.org/img/wn/04n@2x.png"
+                  alt=""
+                  width="36"
+                />
+                <div class="weather-forecast-temperature">
+                  <span class="weather-forecast-temperature-max">4°</span>
+                  <span class="weather-forecast-temperature-min">-7°</span>
+                </div>
+              </div>`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -106,3 +130,4 @@ let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
 search("Ottawa");
+displayForecast();
