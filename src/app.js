230018b2cad4342953147fce744f023eba64020a
mikarogers <96159267+mikarogers@users.noreply.github.com>
2022-02-03 14:38:31 -0800

    Finishing touches

--- src/app.js
+++ src/app.js
@@ -65,7 +65,7 @@ function displayForecast(response) {
                     forecastDay.weather[0].icon
                   }@2x.png"
                   alt=""
-                  width="36"
+                  width="40"
                 />
                 <div class="weather-forecast-temperature">
                   <span class="weather-forecast-temperature-max">${Math.round(
